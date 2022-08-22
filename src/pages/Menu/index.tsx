c1644602ca0dadd0bc2198f55e1454ff615f799b
camilazucchi <camilazucchi@outlook.com>
2022-08-22 17:14:41 -0300

    reset css

--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -1,10 +1,11 @@
 import styles from './Menu.module.scss';
+import {ReactComponent as Logo} from 'assets/logo.svg';
 
 export default function Menu() {
     return (
         <main>
             <nav className={styles.menu}>
-                Japanese Restaurant
+                <Logo />
             </nav>
         </main>
     )
