1dec8874c0e046a96d707264092aa447b0cf4388
camilazucchi <camilazucchi@outlook.com>
2022-09-05 14:51:42 -0300

    alterações na navbar e no redirecionamento de links

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -2,19 +2,32 @@ import {ReactComponent as Logo} from 'assets/logo.svg';
 import styles from './Navbar.module.scss';
 
 export default function Navbar() {
+  const routes =[{
+    label: 'MENU',
+    to: '/menu'
+  }, {
+    label: 'FIND US',
+    to: '/find-us'
+  },{
+    label: 'FAVORITES',
+    to: '/favorites'
+  }];
   return (
     <nav className={styles.navbar}>
       <div className={styles.navbar__logo}>
         <Logo />
       </div>
-      <div className={styles.navbar__links}>
-        <a>MENU</a>
-        <a>FIND US</a>
-        <a>CHEF&#39;S FAVORITE</a>
-      </div>
+      <ul className={styles.navbar}>
+        {routes.map((route, index) => (
+          <li key={index} className={styles.navbar__links}>
+            <a href={route.to}>
+              {route.label}
+            </a>
+          </li>
+        ))}
+      </ul>
       <div className={styles.navbar__button}>
         <button>ORDER NOW</button>
       </div>
     </nav>
-  );
-}
+  );}
